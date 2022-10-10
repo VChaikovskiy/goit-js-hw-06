178e6a85585d4b95b127f31f6538119e61ea403f
VChaikovskiy <ch.v.s.p.i@gmail.com>
2022-10-10 15:46:00 +0300

    task-03

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -12,3 +12,46 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+
+
+const imagesHtml = images.map(image => {
+  return `
+  <li class="card">
+    <img class="img" src="${image.url}" alt="${image.alt}"
+  </li>
+  `
+}).join("");
+
+const galleryEl = document.querySelector('.gallery')
+
+galleryEl.insertAdjacentHTML('afterbegin', imagesHtml);
+
+
+
+console.log()
+
+
+
+
+
+
+
+
+// const galleryArray = [];
+// images.forEach(image => {
+//   const galleryItem = document.createElement('li');
+//   const photoEl = document.createElement('img');
+//   galleryItem.classList.add('card');
+//   photoEl.scr = image.url;
+//   photoEl.alt = image.alt;
+//   galleryItem.append(photoEl);
+//   galleryArray.push(galleryItem);
+
+ 
+// })
+// galleryEl.append(...galleryArray);
+// // galleryEl.insertAdjacentHTML('afterbegin', galleryArray);
+
+// console.log(galleryArray);
+// console.log(galleryEl);

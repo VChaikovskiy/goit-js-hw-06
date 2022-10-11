d6befa28a6410255b1d0ae87d0e5bac93cc21358
VChaikovskiy <ch.v.s.p.i@gmail.com>
2022-10-11 14:26:23 +0300

    task-04

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -38,20 +38,3 @@ console.log()
 
 
 
-// const galleryArray = [];
-// images.forEach(image => {
-//   const galleryItem = document.createElement('li');
-//   const photoEl = document.createElement('img');
-//   galleryItem.classList.add('card');
-//   photoEl.scr = image.url;
-//   photoEl.alt = image.alt;
-//   galleryItem.append(photoEl);
-//   galleryArray.push(galleryItem);
-
- 
-// })
-// galleryEl.append(...galleryArray);
-// // galleryEl.insertAdjacentHTML('afterbegin', galleryArray);
-
-// console.log(galleryArray);
-// console.log(galleryEl);

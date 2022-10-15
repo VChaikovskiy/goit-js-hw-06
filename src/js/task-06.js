98593478a668f37f0aec62c50480d2d80752b67d
VChaikovskiy <ch.v.s.p.i@gmail.com>
2022-10-15 22:08:33 +0300

    task-06

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -1,19 +1,15 @@
-const inputName = document.querySelector('#validation-input');
-inputName.addEventListener('blur', outOfFocusInputName);
-console.dir(inputName);
-//inputName.classList.remove('valid');
-//inputName.classList.remove('invalid');
-function outOfFocusInputName(event) {
-    inputName.classList.remove('valid');
-    inputName.classList.remove('invalid');
-    console.log(event.currentTarget.value);
-    if (event.currentTarget.value.length == inputName.dataset.length) {
-        //inputName.classList.remove('invalid');
-        inputName.classList.add('valid');
+const inputEl = document.querySelector('#validation-input');
+inputEl.addEventListener('blur', onBlurInput);
+
+function onBlurInput (event) {
+    if (event.currentTarget.value.length == inputEl.dataset.length){
+        inputEl.classList.add('valid');
+        inputEl.classList.remove('invalid');
     } else {
-        // inputName.classList.remove('valid');
-        inputName.classList.add('invalid');
-        
+        inputEl.classList.remove('valid');
+        inputEl.classList.add('invalid');
     };
-    return inputName;
+    return inputEl;
 };
+
+console.log(onBlurInput);

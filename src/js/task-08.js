64bd475607f21e1cbe69bca181f917de3a58b129
VChaikovskiy <ch.v.s.p.i@gmail.com>
2022-10-17 10:54:00 +0300

    task-08

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,18 @@
-
+const formLog = document.querySelector('.login-form');
+formLog.addEventListener('submit', onFormSubmit);
+function onFormSubmit(event) {
+    event.preventDefault();
+    const {
+        elements: { email, password },
+    } = event.currentTarget;
+    if (email.value === '' || password.value === '') {
+        alert('Please, fill this form correctly')
+    } else {
+        const formData = {
+            email: email.value,
+            password: password.value,
+        };
+        console.log(formData);
+    }
+    event.currentTarget.reset();
+};

4d826fbf53df0c33f428405de61a2d92d9f730ce
VChaikovskiy <ch.v.s.p.i@gmail.com>
2022-10-17 12:40:46 +0300

    task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+const btnRef = document.querySelector('.change-color');
+const bodyStyle = document.querySelector('body');
+const colorRef = document.querySelector('.color');
+
+
+function getRandomHexColor(event) {
+  const newColor = '#' + Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0);
+  bodyStyle.style.background = newColor;
+  colorRef.textContent = newColor;
+  console.dir(newColor);
+  // return newColor;
+};
+
+btnRef.addEventListener('click', getRandomHexColor);
